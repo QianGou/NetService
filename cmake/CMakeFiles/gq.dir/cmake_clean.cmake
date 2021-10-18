acfef129c3aff6a1df2754da96aeac2c90039663
gq <653382021@qq.com>
2021-10-18 23:53:36 +0800

    cmake

---
+++ cmake/CMakeFiles/gq.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gq.dir/gq/log.cc.o"
+  "../lib/libgq.pdb"
+  "../lib/libgq.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gq.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
